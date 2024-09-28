cc2ec7be42277ad457741640554d006e908b3c7d
aiden <aiden@safuventures.com>
2024-09-28 15:33:11 +0900

    feat: add auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 
+import { AuthProvider } from './context/AuthContext';
 import FeedPage from './pages/Feed';
 import FeedDetailPage from './pages/FeedDetail';
 import LoginPage from './pages/Login';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/feed" element={<FeedPage />} />
-        <Route path="/feed/:uuid" element={<FeedDetailPage />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/feed" element={<FeedPage />} />
+            <Route path="/feed/:uuid" element={<FeedDetailPage />} />
+            <Route path="*" element={<Navigate to="/feed" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
